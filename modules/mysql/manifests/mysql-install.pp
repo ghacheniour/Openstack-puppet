5a8c529920fa0dbadf8b81d2e576ec09403a8046
Ghassen Cheniour <ghacheniour@gmail.com>
2014-04-29 06:58:23 -0700

    adding dependencies between defines and ressources

--- modules/mysql/manifests/mysql-install.pp
+++ modules/mysql/manifests/mysql-install.pp
@@ -1,24 +1,27 @@
 class mysql::mysql-install( ){ 
+  require havana-repository::havana-repository
   $pass = hiera('password')
   $ipaddress = hiera('ipaddress')
   package { [ 'python-mysqldb',
               'mysql-server'   ]:
     ensure => installed,
-  }
+  } 
+  augeas { 'update-bind-address':
+    context => "/files/etc/mysql/my.cnf/target[3]",
+    changes => ["set bind-address $ipaddress"],
+    notify  => Service['mysql'],
+    require => Package['mysql-server', 'python-mysqldb'],
+  }  
   service { 'mysql':
     ensure  => running,
     enable  => true,
     require => Package['python-mysqldb', 'mysql-server'],
-  }
-  augeas { 'update-bind-address':
-  context => "/files/etc/mysql/my.cnf/target[3]",
-  changes => ["set bind-address $ipaddress"],  
-  notify  => Service['mysql'],
-  }
-  
+  } 
   set-password { 'mysql-password':
     password => $pass,
-  }
-  mysql-install-db { 'mysql-install-db': }
+    require  => Service['mysql'],
+  } ->
+  mysql-install-db { 'mysql-install-db': 
+  } -> 
   mysql-secure-installation {'mysql-secure-installation': }
 }
