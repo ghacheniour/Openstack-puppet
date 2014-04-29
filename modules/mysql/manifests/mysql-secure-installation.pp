5d67a68be2bb7776bdf49bcbdecc5402a9d7e390
Ghassen Cheniour <ghacheniour@gmail.com>
2014-04-29 06:58:58 -0700

    rplacing mysql secure installation with sql command

--- modules/mysql/manifests/mysql-secure-installation.pp
+++ modules/mysql/manifests/mysql-secure-installation.pp
@@ -1,6 +1,7 @@
 define mysql-secure-installation() {
+$password = hiera('password')
   exec { 'mysql-secure-installation':
-    command => "/usr/bin/mysql_secure_installation",
+    command => "/usr/bin/mysql -uroot -p$password -e \"DELETE FROM mysql.user WHERE User=''; DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1'); DROP DATABASE IF EXISTS test; FLUSH PRIVILEGES;\" mysql",
     timeout => 0,    
   }
 }
