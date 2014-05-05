0938162a8a2a3079bad34cf8dd7b40b5960120fa
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-05 08:21:46 -0700

    adding keystone classes

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,7 +1,10 @@
 node 'master' {
 #  include puppet
-  class { 'ntp::ntp': }
-  class { 'mysql::mysql-install': }
-  class { 'mysql::mysql-create-db': }
-  class { 'rabbit::rabbitmq': }
+#  class { 'ntp::ntp': }
+#  class { 'mysql::mysql-install': }
+#  class { 'mysql::mysql-create-db': }
+#  class { 'rabbit::rabbitmq': }
+#  class { 'keystone::install::install': }
+class { 'keystone::define::define': }
+class { 'keystone::service-endpoint::service-endpoint': }
 }
