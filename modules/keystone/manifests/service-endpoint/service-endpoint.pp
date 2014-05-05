c4ce0268d39ea9bc1474168e597e7e77838bb627
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-05 08:45:16 -0700

    endpoint function

--- modules/keystone/manifests/service-endpoint/service-endpoint.pp
+++ modules/keystone/manifests/service-endpoint/service-endpoint.pp
@@ -7,12 +7,40 @@ $admin_token = hiera('admin_token')
     admin_token => $admin_token,
     url => $url,
   }  
-  create-endpoint {
+  create-endpoint { 'keystone':
     admin_token => $admin_token, 
-    url         => $url, 
-    service_id  => , 
+    url         => $url,
     publicurl   => "http://$hostname:35357/v2.0", 
     internalurl => "http://$hostname:5000/v2.0",
     adminurl    => "http://$hostname:35357/v2.0",
   }
+  create-endpoint { 'glance':
+    admin_token => $admin_token,
+    url         => $url,
+    publicurl   => "http://$hostname:9292",
+    internalurl => "http://$hostname:9292",
+    adminurl    => "http://$hostname:9292",
+  }
+  create-endpoint { 'nova':
+    admin_token => $admin_token,
+    url         => $url,
+    publicurl   => "http://$hostname::8774/v2/%\(tenant_id\)s ",
+    internalurl => "http://$hostname::8774/v2/%\(tenant_id\)s ",
+    adminurl    => "http://$hostname::8774/v2/%\(tenant_id\)s ",
+  }
+   create-endpoint { 'cinder':
+    admin_token => $admin_token,
+    url         => $url,
+    publicurl   => "http://$hostname:8776/v1/%\(tenant_id\)s",
+    internalurl => "http://$hostname:8776/v1/%\(tenant_id\)s",
+    adminurl    => "http://$hostname:8776/v1/%\(tenant_id\)s",
+  }
+ create-endpoint { 'neutron':
+    admin_token => $admin_token,
+    url         => $url,
+    publicurl   => "http://$hostname:9696 ",
+    internalurl => "http://$hostname:9696 ",
+    adminurl    => "http://$hostname:9696 ",
+  }
+
 }
