13f8dee3f52dd30695e9b086170d966c5d70eeeb
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-05 08:44:43 -0700

    endpoint class

--- modules/keystone/manifests/service-endpoint/create-endpoint.pp
+++ modules/keystone/manifests/service-endpoint/create-endpoint.pp
@@ -1,4 +1,4 @@
-define endpoint ($admin_token, $url, $service_id, $publicurl, $internalurl,$adminurl ){
+define endpoint ($admin_token, $url, $publicurl, $internalurl,$adminurl ){
   exec { "create-ebdpoint-$name":
     command => "/usr/bin/keystone  --os-token=$admin_token --os-endpoint=$url endpoint-create --service-id=$service_id --publicurl=$publicurl $internalurl= $internalurl --adminurl=$adminurl",
     unless => "/usr/bin/keystone  --os-token=$admin_token --os-endpoint=$url endpoint-list | grep $service_id",
