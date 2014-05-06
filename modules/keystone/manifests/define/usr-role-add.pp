dafb288a8ae56407e9e9f7cc60aef7e4414d44cd
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-06 07:01:19 -0700

    deeting unless sentence in the usr-role-addfunction'

--- modules/keystone/manifests/define/usr-role-add.pp
+++ modules/keystone/manifests/define/usr-role-add.pp
@@ -1,6 +1,5 @@
 define usr-role-add ($admin_token, $url, $tenant, $role){
   exec { "add-role-to-usr-$name":
     command => "/usr/bin/keystone  --os-token=$admin_token --os-endpoint=$url user-role-add --user=$name --tenant=$tenant --role=$role",
-    unless => "/usr/bin/keystone  --os-token=$admin_token --os-endpoint=$url user-role-list --user $name --tenant $tenant",
   }
 }
