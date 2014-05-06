0b8824ead1c7e238506b588e536d3bbe6909d25c
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-06 06:58:37 -0700

    finalizing keystone configuration

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,10 +1,13 @@
 node 'master' {
 #  include puppet
-#  class { 'ntp::ntp': }
+  class { 'ntp::ntp': }
 #  class { 'mysql::mysql-install': }
 #  class { 'mysql::mysql-create-db': }
-#  class { 'rabbit::rabbitmq': }
+  class { 'rabbit::rabbitmq': }
 #  class { 'keystone::install::install': }
-class { 'keystone::define::define': }
+#class { 'keystone::define::define': }
 class { 'keystone::service-endpoint::service-endpoint': }
+#class {'keystone::define::test': }
+
+
 }
