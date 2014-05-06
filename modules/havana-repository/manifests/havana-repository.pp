2eb4771e4243e588d77b6534b5497a453ab8974d
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-06 06:59:16 -0700

    updating depnedencies

--- modules/havana-repository/manifests/havana-repository.pp
+++ modules/havana-repository/manifests/havana-repository.pp
@@ -5,20 +5,14 @@ class havana-repository::havana-repository {
     ensure => installed,
   }
   
-  notify { "pyhon software has been succesfully installed": }
-
-  exec { 'add-havna-repository':
+  exec { 'add-havana-repository':
     command => '/usr/bin/add-apt-repository cloud-archive:havana',
     require => Package['python-software-properties'],
   }
   
-  notify { 'cloud archive havana were succefully deployed': }
-  
   exec { 'ubuntu-update':
     command => '/usr/bin/apt-get update && /usr/bin/apt-get -y dist-upgrade',
-    require => Exec['add-havna-repository'],
+    require => Exec['add-havana-repository'],
     timeout => 0,
   }
-    
-  notify { " $::operatingsystem successfully upgraded": }
 }
