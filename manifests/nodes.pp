95ada53f3f87c82be38bba38651ce67322e32896
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-06 09:19:06 -0700

    defining the glance module in the manifests/nodes.pp file

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,13 +1,14 @@
 node 'master' {
 #  include puppet
-  class { 'ntp::ntp': }
+#  class { 'ntp::ntp': }
 #  class { 'mysql::mysql-install': }
 #  class { 'mysql::mysql-create-db': }
-  class { 'rabbit::rabbitmq': }
+#  class { 'rabbit::rabbitmq': }
 #  class { 'keystone::install::install': }
 #class { 'keystone::define::define': }
-class { 'keystone::service-endpoint::service-endpoint': }
-#class {'keystone::define::test': }
+#class { 'keystone::service-endpoint::service-endpoint': }
+class { 'glance::glance-sync': }
+
 
 
 }
