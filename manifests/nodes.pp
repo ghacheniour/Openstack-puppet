a344486d7b8130a3e28a93b9d9eebccc935dc50c
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-07 02:25:49 -0700

    adding nova class to main

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,13 +1,16 @@
 node 'master' {
 #  include puppet
-  class { 'ntp::ntp': }
+#  class { 'ntp::ntp': }
 #  class { 'mysql::mysql-install': }
 #  class { 'mysql::mysql-create-db': }
-  class { 'rabbit::rabbitmq': }
+#  class { 'rabbit::rabbitmq': }
 #  class { 'keystone::install::install': }
 #class { 'keystone::define::define': }
 #class { 'keystone::service-endpoint::service-endpoint': }
-class { 'glance::glance-sync': }
+#class { 'glance::glance-sync': }
+#class { 'nova::nova-install': }
+#class { 'nova::nova-conf': }
+class { 'nova::nova-sync': }
 
 
 
