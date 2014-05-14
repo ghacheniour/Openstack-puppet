f22f0ebe8462b7ff256e8e8b21c5e5898d29d8c9
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-14 07:53:07 -0700

    separate servcie and endpoint

---
+++ modules/keystone/manifests/service-endpoint/service.pp
@@ -0,0 +1,11 @@
+class keystone::service-endpoint::service {
+#require keystone::define::define
+$admin_token = hiera('admin_token')
+  $hostname = hiera('hostname')
+  $url = "http://${hostname}:35357/v2.0"
+  create-service { ['keystone','glance','cinder','nova','neutron','cinderv2']:
+    admin_token => $admin_token,
+    url => $url,
+  }
+
+}
