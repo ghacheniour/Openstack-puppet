10b378daacde72f8e70db9c7b2630b4e2f0430a0
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-14 07:57:01 -0700

    changing require

--- modules/keystone/manifests/service-endpoint/service-endpoint.pp
+++ modules/keystone/manifests/service-endpoint/service-endpoint.pp
@@ -1,5 +1,5 @@
 class keystone::service-endpoint::service-endpoint {
-require keystone::define::define
+#require keystone::define::service
 $admin_token = hiera('admin_token')
   $hostname = hiera('hostname')
   $url = "http://${hostname}:35357/v2.0"
@@ -9,45 +9,35 @@ $admin_token = hiera('admin_token')
   $url_cinder = "http://$hostname:8776/v1/%\(tenant_id\)s"
   $url_neutron =  "http://$hostname:9696/"
   $url_cinderv2 = "http://$hostname:8776/v2/%\(tenant_id\)s"
-  create-service { ['keystone','glance','cinder','nova','neutron','cinderv2']:
-    admin_token => $admin_token,
-    url => $url,
-  }  
   create-endpoint { 'keystone':
     admin_token => $admin_token, 
     url         => $url,
     var         =>  $url_keystone,
-    require     => Create-service['keystone'],
   }
   create-endpoint { 'glance':
     admin_token => $admin_token,
     url         => $url,
     var         => $url_glance,
-    require     => Create-service['glance'],
   }
   create-endpoint { 'nova':
     admin_token => $admin_token,
     url         => $url,
     var         => $url_nova,
-    require     => Create-service['nova'],
   }
    create-endpoint { 'cinder':
     admin_token => $admin_token,
     url         => $url,
     var         => $url_cinder,
-    require     => Create-service['cinder'],
   }
  create-endpoint { 'neutron':
     admin_token => $admin_token,
     url         => $url,
     var         => $url_neutron,
-    require     => Create-service['neutron'],
   }
   create-endpoint { 'cinderv2':
     admin_token => $admin_token,
     url         => $url,
     var         => $url_cinderv2,
-    require     => Create-service['cinderv2'],
   }
 
 }
