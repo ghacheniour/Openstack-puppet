a450727d78db8ad898f8fbf046a5becf27b0cac0
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-14 07:55:56 -0700

    adding dependency

--- modules/nova/manifests/controller/nova-sync.pp
+++ modules/nova/manifests/controller/nova-sync.pp
@@ -2,7 +2,8 @@ class nova::controller::nova-sync {
 require nova::controller::nova-conf
   file { '/var/lib/nova/nova.sqlite':
     ensure => absent,
-  }
+    notify => Nova-manage-db['manage-database'],
+   }
   nova-manage-db { 'manage-database': 
     notify => Service['nova-api',
                       'nova-cert',
