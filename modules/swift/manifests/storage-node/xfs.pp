699d8cc1c85343c26f3000b293204b7ac5a85cdd
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-19 08:57:47 -0700

    xfs ressource

---
+++ modules/swift/manifests/storage-node/xfs.pp
@@ -0,0 +1,15 @@
+define xfs {
+$path = "${name}1"
+exec { "make-xfs-partition-$name":
+  command => "/sbin/mkfs.xfs -f /dev/$path",
+}->
+exec { "echo-$path":
+  command => "/bin/echo \"/dev/$path /srv/node/$path xfs noatime,nodiratime,nobarrier,logbufs=8 0 0\" >> /etc/fstab",
+}->
+file { "/srv/node/$path":
+  ensure => directory,
+}->
+exec { "mount-$name":
+  command => "/bin/mount /srv/node/$path",
+}
+}
