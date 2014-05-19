d864ff929a2f7364db69266faf6600d32d060410
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-19 08:57:14 -0700

    adding /srv/node file

--- modules/swift/manifests/storage-node/disk-partition.pp
+++ modules/swift/manifests/storage-node/disk-partition.pp
@@ -1,3 +1,20 @@
 class swift::storage-node::disk-partition {
-create-disk-partition { '/dev/sdc' : }
+$drivers = hiera_array('swift_drivers')
+file { '/srv':
+  ensure => directory,
+}
+file { '/srv/node/':
+  ensure => directory,
+  recurse => true,
+  require => File['/srv'],
+  owner => 'swift',
+  group => 'swift',
+}
+
+create-disk-partition { $drivers: 
+  } ->
+xfs { $drivers: 
+require => File['/srv/node'],
+}
+
 }
