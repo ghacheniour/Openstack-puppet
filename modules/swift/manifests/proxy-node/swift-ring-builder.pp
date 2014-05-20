b8c6d06b2697e0849afb5f2e21f3313706553bbb
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-20 07:02:00 -0700

    defining ring rebalance

--- modules/swift/manifests/proxy-node/swift-ring-builder.pp
+++ modules/swift/manifests/proxy-node/swift-ring-builder.pp
@@ -2,7 +2,8 @@ class swift::proxy-node::swift-ring-builder {
 #swift-ring-builder-create { ['account', 'container', 'object']: }
 $swift_drivers= hiera_array('swift_drivers')
 #swift-ring-account-builder-add { $swift_drivers: }
-swift-ring-container-builder-add { $swift_drivers: }
-swift-ring-object-builder-add { $swift_drivers: }
+#swift-ring-container-builder-add { $swift_drivers: }
+#swift-ring-object-builder-add { $swift_drivers: }
+ swift-ring-builder-rebalance { ['account', 'container', 'object']: }
 
 }
