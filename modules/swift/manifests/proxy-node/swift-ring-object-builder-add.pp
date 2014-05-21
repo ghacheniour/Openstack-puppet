3edb4b947b5524b4c2edbd32916530fbf2a6f0ef
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-21 07:35:24 -0700

    updating port

--- modules/swift/manifests/proxy-node/swift-ring-object-builder-add.pp
+++ modules/swift/manifests/proxy-node/swift-ring-object-builder-add.pp
@@ -5,7 +5,7 @@ $swift_drivers = hiera_array('swift_drivers')
 $n = inline_template('<%= @swift_drivers.index(@name)+1 %>')
 $zone = "z${n}"
 exec { "swift-ring-object-add-$name":
-  command => "/usr/bin/swift-ring-builder object.builder add $zone-$storage_ipaddress:6001/$device 100",
+  command => "/usr/bin/swift-ring-builder object.builder add $zone-$storage_ipaddress:6000/$device 100",
   cwd     => '/etc/swift',
 }
 }
