b0d28a26a1399e4b805456a4134cead6d3935535
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-21 08:03:00 -0700

    update cwd

--- modules/swift/manifests/proxy-node/swift-ring-builder-create.pp
+++ modules/swift/manifests/proxy-node/swift-ring-builder-create.pp
@@ -2,6 +2,6 @@ define swift-ring-builder-create {
 $builder = "${name}.builder"
 exec { "swift-ring-builder-create-$name":
   command => "/usr/bin/swift-ring-builder $builder create 18 3 1",
-  cwd     => "/etc/swift/",
+  cwd     => "/etc/swift",
 }
 }
