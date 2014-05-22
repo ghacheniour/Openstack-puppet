7fdc4c40d4aaf88a5315acf5eb508181aa2eead2
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-22 08:38:40 -0700

    adding comments

--- modules/mysql/manifests/mysql-install-db.pp
+++ modules/mysql/manifests/mysql-install-db.pp
@@ -1,5 +1,6 @@
-define mysql-install-db () {
+#execute mysql_install_db command
+define mysql-install-db {
   exec { 'mysql-install-db':
-    command => "/usr/bin/mysql_install_db",
+    command => '/usr/bin/mysql_install_db',
   }
 }
