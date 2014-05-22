11137e27647ffbe9b4d1c960ae937befb1337be8
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-22 08:39:38 -0700

    adding comments

--- modules/mysql/manifests/mysql-secure-installation.pp
+++ modules/mysql/manifests/mysql-secure-installation.pp
@@ -1,7 +1,9 @@
+# this define execute the mysql secure installation I have used the 
+# definition of this command
 define mysql-secure-installation() {
 $password = hiera('password')
   exec { 'mysql-secure-installation':
     command => "/usr/bin/mysql -uroot -p$password -e \"DELETE FROM mysql.user WHERE User=''; DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1'); DROP DATABASE IF EXISTS test; FLUSH PRIVILEGES;\" mysql",
-    timeout => 0,    
+    timeout => 0,
   }
 }
