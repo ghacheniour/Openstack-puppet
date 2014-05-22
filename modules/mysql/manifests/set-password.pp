84a40eb95e3b46d40776d373f25e66bf793f80cd
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-22 08:38:56 -0700

    adding comments

--- modules/mysql/manifests/set-password.pp
+++ modules/mysql/manifests/set-password.pp
@@ -1,6 +1,6 @@
-define set-password($password) { 
+define set-password($password) {
   exec { "set-${name}":
-    unless => "/usr/bin/mysqladmin -uroot -p${password} status",
+    unless  => "/usr/bin/mysqladmin -uroot -p${password} status",
     command => "/usr/bin/mysqladmin -uroot password ${password}",
     require => Service['mysql'],
   }
