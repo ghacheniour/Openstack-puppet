2053132e3593cf7eb89ad7981db1bce437d5acfe
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-22 09:02:55 -0700

    add comments

--- modules/keystone/manifests/install/mysql.pp
+++ modules/keystone/manifests/install/mysql.pp
@@ -1,9 +1,10 @@
+# this define writes keystone sql in the keystone.conf file
 define mysql ($module) {
   $hostname = hiera('hostname')
   $database = hiera_hash('database')
   $url = " mysql://${database[$module][user]}:${database[$module][password]}@${hostname}/${module}"
   augeas { $name:
-    context => "/files/etc/keystone/keystone.conf/sql",
+    context => '/files/etc/keystone/keystone.conf/sql',
     changes => ["set connection $url"],
   }
 }
