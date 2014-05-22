8799923a769678b875bd0ae1b79cad942d146fa7
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-22 09:02:04 -0700

    add comments

--- modules/keystone/manifests/define/usr-role-add.pp
+++ modules/keystone/manifests/define/usr-role-add.pp
@@ -1,3 +1,4 @@
+# this define add roles to users
 define usr-role-add ($admin_token, $url, $tenant, $role){
   exec { "add-role-to-usr-$name":
     command => "/usr/bin/keystone  --os-token=$admin_token --os-endpoint=$url user-role-add --user=$name --tenant=$tenant --role=$role",
