6641ff3f5667e63d0bed624c75eb80a6099e845f
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-22 08:39:23 -0700

    adding comments

--- modules/mysql/manifests/mysql-install.pp
+++ modules/mysql/manifests/mysql-install.pp
@@ -1,27 +1,27 @@
-class mysql::mysql-install( ){ 
-#  require havana-repository::havana-repository
+#this install and configure mysql server
+class mysql::mysql-install( ){
   $pass = hiera('password')
   $ipaddress = hiera('ipaddress')
   package { [ 'python-mysqldb',
               'mysql-server'   ]:
     ensure => installed,
-  } 
+  }
   augeas { 'update-bind-address':
-    context => "/files/etc/mysql/my.cnf/target[3]",
-    changes => ["set bind-address $ipaddress"],
+    context => '/files/etc/mysql/my.cnf/target[3]',
+    changes => "set bind-address $ipaddress",
     notify  => Service['mysql'],
     require => Package['mysql-server', 'python-mysqldb'],
-  }  
+  }
   service { 'mysql':
     ensure  => running,
     enable  => true,
     require => Package['python-mysqldb', 'mysql-server'],
-  } 
+  }
   set-password { 'mysql-password':
     password => $pass,
     require  => Service['mysql'],
   } ->
-  mysql-install-db { 'mysql-install-db': 
-  } -> 
+  mysql-install-db { 'mysql-install-db':
+  } ->
   mysql-secure-installation {'mysql-secure-installation': }
 }
