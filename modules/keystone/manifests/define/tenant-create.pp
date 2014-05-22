470dcfc97226357bb304a120df48a0cffeb8291f
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-22 09:01:32 -0700

    add comments

--- modules/keystone/manifests/define/tenant-create.pp
+++ modules/keystone/manifests/define/tenant-create.pp
@@ -1,4 +1,5 @@
-define tenant-create ($admin_token, $url, $tenant_name, $desc) { 
+# this define creates defines
+define tenant-create ($admin_token, $url, $tenant_name, $desc) {
   exec { $name:
     command => "/usr/bin/keystone --os-token=$admin_token --os-endpoint=$url tenant-create --name=$tenant_name --description=\"$desc\"",
     unless => "/usr/bin/keystone  --os-token=$admin_token --os-endpoint=$url tenant-list |grep $tenant_name",
