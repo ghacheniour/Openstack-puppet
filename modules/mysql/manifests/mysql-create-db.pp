4f4ecda8d2abb4a360f6dee2d4fd07c8eb1d9d61
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-22 08:38:29 -0700

    adding comments

--- modules/mysql/manifests/mysql-create-db.pp
+++ modules/mysql/manifests/mysql-create-db.pp
@@ -1,9 +1,10 @@
+#class that creates databeses for all openstack services
 class mysql::mysql-create-db () {
-require mysql::mysql-install
-create-db { 'cinder': }
-create-db { 'neutron': }
-create-db { 'glance': }
-create-db { 'keystone': }
-create-db { 'nova': }
-create-db { 'heat': }
+  require mysql::mysql-install
+  create-db { 'cinder': }
+  create-db { 'neutron': }
+  create-db { 'glance': }
+  create-db { 'keystone': }
+  create-db { 'nova': }
+  create-db { 'heat': }
 }
