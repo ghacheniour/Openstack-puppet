cab72ca59307dafff00e26065d7d92b13bab1d35
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-22 09:02:43 -0700

    add comments

--- modules/keystone/manifests/install/manage-database.pp
+++ modules/keystone/manifests/install/manage-database.pp
@@ -1,5 +1,6 @@
+# this define manage database
 define manage-database {
   exec { $name:
-    command => "/usr/bin/keystone-manage db_sync",
+    command => '/usr/bin/keystone-manage db_sync',
   }
 }
