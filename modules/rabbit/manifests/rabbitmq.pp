6bc4e8d5d1323ad30a2ce671ab29050b4e26abfa
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-22 08:08:38 -0700

    class commented

--- modules/rabbit/manifests/rabbitmq.pp
+++ modules/rabbit/manifests/rabbitmq.pp
@@ -1,8 +1,9 @@
+#this class install rabbit-mq server and start the service
 class rabbit::rabbitmq {
   package { 'rabbitmq-server' : ensure => installed, }
   service { 'rabbitmq-server':
     ensure  => running,
     require => Package['rabbitmq-server'],
-    enable => true,
+    enable  => true,
   }
 }
