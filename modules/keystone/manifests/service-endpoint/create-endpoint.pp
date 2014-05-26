40b98d88769b64bf1d78a51621b0e71d683a2cd6
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-26 07:00:44 -0700

    change unless statement

--- modules/keystone/manifests/service-endpoint/create-endpoint.pp
+++ modules/keystone/manifests/service-endpoint/create-endpoint.pp
@@ -1,6 +1,6 @@
 define create-endpoint ($admin_token, $url, $var){
   exec { "create-ebdpoint-$name":
     command => "/usr/bin/keystone  --os-token=$admin_token --os-endpoint=$url endpoint-create  --publicurl=$var --internalurl=$var --adminurl=$var --service=$name",
-    unless => "/usr/bin/keystone  --os-token=$admin_token --os-endpoint=$url endpoint-list | grep $id",
+    unless => "/usr/bin/keystone  --os-token=$admin_token --os-endpoint=$url endpoint-list | grep $var",
   }
 }
