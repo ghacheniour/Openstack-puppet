2108a66c757378ab00e05edb0602cc6ac1ee276b
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-26 00:34:24 -0700

    replacing ruby function with inline_template

--- modules/keystone/manifests/service-endpoint/create-endpoint.pp
+++ modules/keystone/manifests/service-endpoint/create-endpoint.pp
@@ -1,5 +1,6 @@
 define create-endpoint ($admin_token, $url, $var){
-$id = id($admin_token, $url, $name)
+$service = inline_template('<%= %x{/usr/bin/keystone --os-token=#{admin_token}  --os-endpoint=#{url}  service-list  | /bin/grep #{name}} %>')
+$id = inline_template("<%= @service.slice(2,32) %>")
   exec { "create-ebdpoint-$name":
     command => "/usr/bin/keystone  --os-token=$admin_token --os-endpoint=$url endpoint-create  --publicurl=$var --internalurl=$var --adminurl=$var --service-id=$id",
     unless => "/usr/bin/keystone  --os-token=$admin_token --os-endpoint=$url endpoint-list | grep $id",
