399607e6bf3f740ac70ebef0157e721dc4aa42bf
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-26 06:26:21 -0700

    endpoint function

--- modules/keystone/manifests/service-endpoint/create-endpoint.pp
+++ modules/keystone/manifests/service-endpoint/create-endpoint.pp
@@ -1,8 +1,6 @@
 define create-endpoint ($admin_token, $url, $var){
-$service = inline_template('<%= %x{/usr/bin/keystone --os-token=#{admin_token}  --os-endpoint=#{url}  service-list  | /bin/grep #{name}} %>')
-$id = inline_template("<%= @service.slice(2,32) %>")
   exec { "create-ebdpoint-$name":
-    command => "/usr/bin/keystone  --os-token=$admin_token --os-endpoint=$url endpoint-create  --publicurl=$var --internalurl=$var --adminurl=$var --service-id=$id",
+    command => "/usr/bin/keystone  --os-token=$admin_token --os-endpoint=$url endpoint-create  --publicurl=$var --internalurl=$var --adminurl=$var --service=$name",
     unless => "/usr/bin/keystone  --os-token=$admin_token --os-endpoint=$url endpoint-list | grep $id",
   }
 }
