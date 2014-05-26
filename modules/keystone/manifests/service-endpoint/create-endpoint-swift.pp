fc1b91d7ae396d2da7fffbfe1bb4964c18720422
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-26 06:58:30 -0700

    change edpointswift

--- modules/keystone/manifests/service-endpoint/create-endpoint-swift.pp
+++ modules/keystone/manifests/service-endpoint/create-endpoint-swift.pp
@@ -1,8 +1,6 @@
 define create-endpoint-swift ($admin_token, $url, $var, $var2){
-$service = inline_template('<%= %x{/usr/bin/keystone --os-token=#{admin_token} --os-endpoint=#{url} service-list | /bin/grep #{name}} %>')
-$id = inline_template("<%= @service.slice(2,32) %>")
   exec { "create-endpoint-$name":
-    command => "/usr/bin/keystone  --os-token=$admin_token --os-endpoint=$url endpoint-create  --publicurl=$var --internalurl=$var --adminurl=$var2 --service-id=$id",
-    unless => "/usr/bin/keystone  --os-token=$admin_token --os-endpoint=$url endpoint-list | grep $id",
+    command => "/usr/bin/keystone  --os-token=$admin_token --os-endpoint=$url endpoint-create  --publicurl=$var --internalurl=$var --adminurl=$var2 --service=$name",
+    unless => "/usr/bin/keystone  --os-token=$admin_token --os-endpoint=$url endpoint-list | grep $var",
   }
 }
