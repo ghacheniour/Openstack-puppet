b9c17103e5e1a44957e25fbf1466d72d6dca942f
Ghassen Cheniour <ghacheniour@gmail.com>
2014-05-26 03:18:01 -0700

    editing servcie

--- modules/keystone/manifests/service-endpoint/service.pp
+++ modules/keystone/manifests/service-endpoint/service.pp
@@ -3,7 +3,7 @@ class keystone::service-endpoint::service {
   $admin_token = hiera('admin_token')
   $hostname = hiera('hostname')
   $url = "http://${hostname}:35357/v2.0"
-  create-service { ['keystone','glance','cinder','nova','neutron','cinderv2','swift','heat', 'heat-cfn']:
+  create-service { ['keystone', 'glance', 'cinder', 'nova', 'neutron', 'cinderv2', 'swift', 'heat', 'heat-cfn']:
     admin_token => $admin_token,
     url => $url,
   }
